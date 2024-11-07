120ff1580710f5c54ab3082d6693fdebdf7208c9
thebigcoderheehee <singhharfateh47@gmail.com>
2024-11-06 23:19:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const texts = ["FREE SHIPPING ON ORDERS $60+", "SEE DETAILS", "FREE AND EASY RETURNS", "SEE DETAILS"];
+let index = 0;
+const flashingText = document.getElementById("flashing-text");
+function flashText() {
+    flashingText.style.opacity = "0";
+    setTimeout(() => {
+      flashingText.innerText = texts[index];
+      flashingText.style.opacity = "1";
+      index = (index + 1) % texts.length; 
+    }, 500); 
+  }
+  
+  setInterval(flashText, 4000);
