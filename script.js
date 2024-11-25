dc2cd12e55736f6ce19bcf7b2b02bff620f78907
thebigcoderheehee <singhharfateh47@gmail.com>
2024-11-24 19:31:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const texts = ["FREE SHIPPING ON ORDERS $60+", "SEE DETAILS", "FREE AND EASY RETURNS", "SEE DETAILS"];
 let index = 0;
 const flashingText = document.getElementById("flashing-text");
+
 function flashText() {
-    flashingText.style.opacity = "0";
-    setTimeout(() => {
-      flashingText.innerText = texts[index];
-      flashingText.style.opacity = "1";
-      index = (index + 1) % texts.length; 
-    }, 500); 
-  }
-  
-  setInterval(flashText, 4000);
+  flashingText.style.opacity = "0";
+  setTimeout(() => {
+    flashingText.innerText = texts[index];
+    flashingText.style.opacity = "1";
+    index = (index + 1) % texts.length;
+  }, 500);
+}
+
+setInterval(flashText, 4000);
+
+// hover effect
+
+document.querySelector('.modal-hover').addEventListener('mouseenter', function () {
+  let myModal = new bootstrap.Modal(document.getElementById('modalExample'), {});
+  myModal.show();
+});
